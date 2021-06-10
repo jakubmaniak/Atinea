49790035aa773a5f85c998a128229533086f8281
Jakub Maniak <jakubmaniak99@gmail.com>
2021-06-10 21:29:29 +0200

    Add both algorithm implementations

---
+++ ImplementacjaRekurencyjna/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace RekrutacjaAtineaRekurencyjnie
+{
+    class Program
+    {
+        internal class Building
+        {
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+
+            public Building(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Building[] buildings = ReadInput();
+            int posterCount = 0;
+            CheckRegion(buildings, 0, buildings.Length - 1, 0, ref posterCount);
+
+            Console.WriteLine(posterCount);
+        }
+
+        private static Building[] ReadInput()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Building[] buildings = new Building[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] parts = Console.ReadLine().Split(' ');
+                buildings[i] = new Building(int.Parse(parts[0]), int.Parse(parts[1]));
+            }
+
+            return buildings;
+        }
+
+        private static int CalculateRegionHeight(Building[] buildings, int fromIndex, int toIndex, int fromY)
+        {
+            int regionHeight = buildings[fromIndex].Height - fromY;
+
+            for (int i = fromIndex + 1; i <= toIndex; i++)
+            {
+                Building current = buildings[i];
+                if (current.Height < regionHeight + fromY)
+                {
+                    regionHeight = current.Height - fromY;
+                }
+            }
+
+            return regionHeight;
+        }
+
+
+        private static void CheckRegion(Building[] buildings, int fromIndex, int toIndex, int fromY, ref int posterCount)
+        {
+            int regionHeight = CalculateRegionHeight(buildings, fromIndex, toIndex, fromY);
+            int? branchFromIndex = null;
+
+            for (int i = fromIndex; i <= toIndex; i++)
+            {
+                if (buildings[i].Height - fromY > regionHeight)
+                {
+                    if (branchFromIndex == null) branchFromIndex = i;
+                }
+                else if (branchFromIndex != null)
+                {
+                    CheckRegion(buildings, (int)branchFromIndex, i - 1, fromY + regionHeight, ref posterCount);
+                    branchFromIndex = null;
+                }
+            }
+
+            if (branchFromIndex != null)
+            {
+                CheckRegion(buildings, (int)branchFromIndex, toIndex, fromY + regionHeight, ref posterCount);
+            }
+
+            posterCount++;
+        }
+    }
+}
---
+++ ImplementacjaWzorcowa/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RekrutacjaAtinea
+{
+    class Program
+    {
+        internal class Building
+        {
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+
+            public Building(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Building[] buildings = ReadInput();
+            int posterCount = CalculatePosterCount(buildings);
+
+            Console.WriteLine(posterCount);
+        }
+
+        private static Building[] ReadInput()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Building[] buildings = new Building[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] parts = Console.ReadLine().Split(' ');
+                buildings[i] = new Building(int.Parse(parts[0]), int.Parse(parts[1]));
+            }
+
+            return buildings;
+        }
+
+        private static int CalculatePosterCount(Building[] buildings)
+        {
+            int posterCount = 0;
+            Stack<int> stack = new Stack<int>();
+
+            foreach (Building building in buildings)
+            {
+                while (stack.Count != 0 && stack.Peek() > building.Height)
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count == 0 || stack.Peek() < building.Height)
+                {
+                    stack.Push(building.Height);
+                    posterCount++;
+                }
+            }
+
+            return posterCount;
+        }
+    }
+}
